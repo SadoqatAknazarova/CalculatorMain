1d24a6dd532012eddd8775483b2b6eb81a8b39ea
PC673 <odilaknazarov@gmail.com>
2023-01-26 09:01:29 +0500

    backend

--- app/src/main/java/uz/itschool/calculator/MainActivity.kt
+++ app/src/main/java/uz/itschool/calculator/MainActivity.kt
@@ -9,31 +9,33 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.text.isDigitsOnly
 
 class MainActivity() : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var one:Button
-    private lateinit var two:Button
-    private lateinit var three:Button
-    private lateinit var four:Button
-    private lateinit var five:Button
-    private lateinit var six:Button
-    private lateinit var seven:Button
-    private lateinit var eight:Button
-    private lateinit var nine:Button
-    private lateinit var zero:Button
+    private lateinit var one: Button
+    private lateinit var two: Button
+    private lateinit var three: Button
+    private lateinit var four: Button
+    private lateinit var five: Button
+    private lateinit var six: Button
+    private lateinit var seven: Button
+    private lateinit var eight: Button
+    private lateinit var nine: Button
+    private lateinit var zero: Button
 
-    private lateinit var multiply:Button
-    private lateinit var plus:Button
-    private lateinit var delete:Button
-    private lateinit var minus:Button
-    private lateinit var dot:Button
-    private lateinit var div:Button
-    private lateinit var clear:Button
-    private lateinit var oper:TextView
-    private lateinit var result:TextView
-    private var ispoint=true
-    private var issymbol=false
+    private lateinit var percent:Button
+    private lateinit var multiply: Button
+    private lateinit var plus: Button
+    private lateinit var delete: Button
+    private lateinit var minus: Button
+    private lateinit var dot: Button
+    private lateinit var div: Button
+    private lateinit var clear: Button
+    private lateinit var oper: TextView
+    private lateinit var result: TextView
+    private var ispoint = true
+    private var issymbol = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,131 +44,128 @@ class MainActivity() : AppCompatActivity(), View.OnClickListener {
         initUI()
 
 
-    one.setOnClickListener(this)
-    two.setOnClickListener(this)
-    three.setOnClickListener(this)
-    four.setOnClickListener(this)
-    five.setOnClickListener(this)
-    six.setOnClickListener(this)
-    seven.setOnClickListener(this)
-    eight.setOnClickListener(this)
-    nine.setOnClickListener(this)
-    zero.setOnClickListener(this)
+        one.setOnClickListener(this)
+        two.setOnClickListener(this)
+        three.setOnClickListener(this)
+        four.setOnClickListener(this)
+        five.setOnClickListener(this)
+        six.setOnClickListener(this)
+        seven.setOnClickListener(this)
+        eight.setOnClickListener(this)
+        nine.setOnClickListener(this)
+        zero.setOnClickListener(this)
 
         dot.setOnClickListener {
-            if (ispoint){
+            if (ispoint) {
+                if (oper.text.substring(oper.text.length-1,oper.text.length).isDigitsOnly())
 
-                oper.text=oper.text.toString()+"."
-                ispoint=false }
-          }
-
+                oper.text = oper.text.toString() + "."
+                ispoint = false
+            }
+        }
 
         clear.setOnClickListener {
-            oper.text="0" }
+            oper.text = "0"
+        }
 
-
+        delete.setOnClickListener {
+            oper.text=oper.text.substring(0,oper.text.length-1)
+        }
 
         multiply.setOnClickListener {
             addSymbol("x")
         }
 
         div.setOnClickListener {
-           addSymbol("/")
+            addSymbol("/")
+        }
+        percent.setOnClickListener {
+            addSymbol("%")
         }
         plus.setOnClickListener {
             addSymbol(plus.text.toString())
         }
         minus.setOnClickListener {
-            addSymbol(minus.text.toString())
+            addSymbol("-")
         }
 
-}
-
-
-
-
-
-
-
-
-
+    }
 
 
     override fun onClick(p0: View?) {
-val btn=findViewById<Button>(p0!!.id)
-        if (oper.text!="0")
-        oper.text=oper.text.toString()+btn.text
+        val btn = findViewById<Button>(p0!!.id)
+        if (oper.text != "0")
+            oper.text = oper.text.toString() + btn.text
         else
-            oper.text=btn.text
-        issymbol= true
+            oper.text = btn.text
+        issymbol = true
     }
 
 
-private fun calculate():String{
-var r="0"
-    createArray(oper.text.toString())
-    return r
-}
-
-
-fun addSymbol(symbol:String){
-    if (issymbol){
-        oper.text=oper.text.toString()+symbol
-        issymbol=false
+    private fun calculate(): String {
+        var r = "0"
+        createArray(oper.text.toString())
+        return r
     }
-    else{
-        if (oper.text!="0"){
-            oper.text=oper.text.substring(0,oper.text.length-1)+symbol
-    }}
-    ispoint=false
 
-}
-fun createArray(s:String):MutableList<Any>{
-    var list= mutableListOf<Any>()
 
-    var temp=""
-    for(i in s)
-        if (i.isDigit() || i =='.'){
-            temp+=i
+    fun addSymbol(symbol: String) {
+        if (issymbol) {
+            oper.text = oper.text.toString() + symbol
+            issymbol = false
+        } else {
+            if (oper.text != "0") {
+                oper.text = oper.text.substring(0, oper.text.length - 1) + symbol
+            }
         }
-            else{
+        ispoint = true
+
+    }
+
+    fun createArray(s: String): MutableList<Any> {
+        var list = mutableListOf<Any>()
+
+        var temp = ""
+        for (i in s)
+            if (i.isDigit() || i == '.') {
+                temp += i
+            } else {
                 list.add(temp)
-            list.add(i)
-            temp=""
-        }
-    if (temp.isNotEmpty()){
-        list.add(temp)
+                list.add(i)
+                temp = ""
+            }
+        if (temp.isNotEmpty()) {
+            list.add(temp)
 
-    }
+        }
 
         return list
 
-}
-
-
-
-    fun initUI(){
-        div=findViewById(R.id.div)
-        multiply=findViewById(R.id.multiply)
-        clear=findViewById(R.id.clear)
-        oper=findViewById(R.id.oper)
-        result=findViewById(R.id.result)
-        dot=findViewById(R.id.dot)
-        one=findViewById(R.id.one)
-        two=findViewById(R.id.two)
-        three=findViewById(R.id.three)
-        four=findViewById(R.id.four)
-        five=findViewById(R.id.five)
-        six=findViewById(R.id.six)
-        seven=findViewById(R.id.seven)
-        eight=findViewById(R.id.eight)
-        nine=findViewById(R.id.nine)
-        zero=findViewById(R.id.zero)
-        plus=findViewById(R.id.plus)
-        minus=findViewById(R.id.minus)
-        delete=findViewById(R.id.delete)
     }
 
+
+    fun initUI() {
+        percent=findViewById(R.id.percent)
+        div = findViewById(R.id.div)
+        multiply = findViewById(R.id.multiply)
+        clear = findViewById(R.id.clear)
+        oper = findViewById(R.id.oper)
+        result = findViewById(R.id.result)
+        dot = findViewById(R.id.dot)
+        one = findViewById(R.id.one)
+        two = findViewById(R.id.two)
+        three = findViewById(R.id.three)
+        four = findViewById(R.id.four)
+        five = findViewById(R.id.five)
+        six = findViewById(R.id.six)
+        seven = findViewById(R.id.seven)
+        eight = findViewById(R.id.eight)
+        nine = findViewById(R.id.nine)
+        zero = findViewById(R.id.zero)
+        plus = findViewById(R.id.plus)
+        minus = findViewById(R.id.minus)
+        delete = findViewById(R.id.delete)
+    }
 
 
 }
